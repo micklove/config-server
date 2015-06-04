01ba8f98f3e06198bf022da810fe929cdeb67a78
micklove <micklove+github@gmail.com>
2015-06-05 00:46:46 +1000

    Update service config uris Update pom, with spring-cloud-starter-config Remove redundant @Config and @EnableAutoConfiguration (as @SpringBootApplication covers both of these...)

--- config-service/src/main/java/mlo55/ConfigurationServerApplication.java
+++ config-service/src/main/java/mlo55/ConfigurationServerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//@Configuration
-//@EnableAutoConfiguration
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServerApplication {
--- echo-service/src/main/java/mlo55/EchoServerApplication.java
+++ echo-service/src/main/java/mlo55/EchoServerApplication.java
@@ -3,8 +3,6 @@ package mlo55;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@Configuration
-//@EnableAutoConfiguration
 @SpringBootApplication
 public class EchoServerApplication {
 
